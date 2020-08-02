2d82c1e20e10ca823a6760996d0ae0dca9205b66
athlaliel <athla8452@gmail.com>
2020-08-02 16:26:50 +0900

    ajax-complete
    jqueryを導入し、非同期通信をコメントに導入完了

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,33 @@
+$(function(){
+  function buildHTML(comment){
+    let html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>：
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+})
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+    end
   end
 
   private
