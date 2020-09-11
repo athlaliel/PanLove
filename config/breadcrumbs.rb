c47fa250bb838035ddfeb24e6f54df7ba6f200de
athlaliel <athla8452@gmail.com>
2020-09-11 11:35:38 +0900

    gretel_update

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,30 +2,6 @@ crumb :root do
   link "Home", root_path
 end
 
-# user#index
-crumb :users do
-  link "Users", users_path
-  parent :root
-end
-
-# user#show
-crumb :show_user do |user|
-  link user.name, user
-  parent :users
-end
-
-# user#edit
-crumb :edit_user do |user|
-  link "Edit #{user.name}", edit_user_path(user)
-  parent :show_user, user
-end
-
-# user#new
-crumb :new_user do
-  link "New User", new_user_path
-  parent :users
-end
-
 # # マイページ
 # crumb :mypage do
 #   link "マイページ", user_path
