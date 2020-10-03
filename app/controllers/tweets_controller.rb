60edc4383c8ab4db4f6eec75f95d6508914bce86
athlaliel <athla8452@gmail.com>
2020-10-03 23:24:41 +0900

    view_list_change

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show, :search]
 
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC").all.page(params[:page]).per(3)
+    @tweets = Tweet.includes(:user).order("created_at DESC").all.page(params[:page]).per(8)
   end
 
   def new
